1cd16643fff61e75560880573e7fecb001775af4
Alin Cadariu <alincadariu@Alins-MacBook-Pro.local>
2020-04-30 17:34:48 +0300

    make columns and rows random

--- challenges/2/index.ts
+++ challenges/2/index.ts
@@ -1,19 +1,36 @@
 import { generateMatrice } from './src/generateMatrice';
 import { MatriceRenderer } from './src/MatriceRenderer';
 
-const COLUMNS: number = 5;
-const ROWS: number = 6;
+let COLUMNS: number;
+let ROWS: number;
 
-const handle = setInterval(function() {
+document.body.addEventListener('click', ev => {
+    if (!(ev.target instanceof HTMLElement)) return;
 
-    while(document.body.lastChild) { 
-        document.body.removeChild(document.body.lastChild); 
-    }
+    const isCell = ev.target.dataset['column'] != null && ev.target.dataset['row'] != null;
+    if (!isCell) return;
 
-    const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    console.log({
+        x: ev.target.dataset['column'],
+        y: ev.target.dataset['row']
+    });
+})
+
+function render() {
+
+    document.body.innerHTML = '';
+    COLUMNS = Math.floor(Math.random() * 10 + 1);
+    ROWS = Math.floor(Math.random() * 30 + 1);
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     const matrice = generateMatrice(ROWS, COLUMNS);
     const renderer = new MatriceRenderer(matrice);
     renderer.render = randomColor;
-    
-}, 6000);
+
+}
+
+
+render();
+
+//const handle = setInterval(render, 6000);
+
 
